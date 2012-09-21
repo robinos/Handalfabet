0052e999707213091d7421dbc3eab20f0d2a666b
robinos <robin.l.osborne@gmail.com>
2012-09-21 11:49:05 +0200

    En väldig enkel game window, behöver mycket arbete

--- Android/gen/com/example/android/R.java
+++ Android/gen/com/example/android/R.java
@@ -11,16 +11,39 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int first_button=0x7f020000;
+        public static final int fourth_button=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int next_letter_button=0x7f020004;
+        public static final int next_letter_button_pushed=0x7f020005;
+        public static final int second_button=0x7f020006;
+        public static final int third_button=0x7f020007;
     }
     public static final class id {
-        public static final int analogClock1=0x7f070004;
+        public static final int analogClock1=0x7f070015;
         public static final int backButton=0x7f070003;
-        public static final int back_Button=0x7f070005;
-        public static final int highScoreButton=0x7f070002;
+        public static final int back_Button=0x7f070016;
+        public static final int first_opt_button=0x7f070004;
+        public static final int fourth_opt_button=0x7f070006;
+        public static final int highScoreButton=0x7f070014;
         public static final int loginButton=0x7f070001;
-        public static final int menu_settings=0x7f070006;
+        public static final int loginView=0x7f070002;
+        public static final int menu_settings=0x7f070017;
+        public static final int next_letter_button=0x7f070012;
+        public static final int point_view1=0x7f07000b;
+        public static final int point_view2=0x7f07000c;
+        public static final int point_view3=0x7f07000d;
+        public static final int point_view4=0x7f07000e;
+        public static final int progressBar=0x7f07000a;
+        public static final int questionView1=0x7f070007;
+        public static final int questionView2=0x7f070009;
+        public static final int questionView3=0x7f070008;
+        public static final int second_opt_button=0x7f070005;
+        public static final int settings_button=0x7f070011;
+        public static final int third_opt_button=0x7f070013;
+        public static final int total_point_view1=0x7f07000f;
+        public static final int total_point_view2=0x7f070010;
         public static final int username=0x7f070000;
     }
     public static final class layout {
@@ -34,11 +57,26 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040004;
         public static final int back=0x7f040000;
+        public static final int first_opt_button=0x7f040009;
+        public static final int fourth_opt_button=0x7f04000c;
         public static final int hello_world=0x7f040005;
         public static final int highScore=0x7f040001;
         public static final int login=0x7f040003;
+        public static final int login_view=0x7f040008;
         public static final int menu_settings=0x7f040006;
+        public static final int next_letter_button=0x7f040016;
+        public static final int point_view1=0x7f040010;
+        public static final int point_view2=0x7f040011;
+        public static final int point_view3=0x7f040012;
+        public static final int point_view4=0x7f040013;
+        public static final int question_view1=0x7f04000d;
+        public static final int question_view2=0x7f04000e;
+        public static final int question_view3=0x7f04000f;
+        public static final int second_opt_button=0x7f04000a;
+        public static final int third_opt_button=0x7f04000b;
         public static final int title_activity_main=0x7f040007;
+        public static final int total_point_view1=0x7f040014;
+        public static final int total_point_view2=0x7f040015;
         public static final int username=0x7f040002;
     }
     public static final class style {

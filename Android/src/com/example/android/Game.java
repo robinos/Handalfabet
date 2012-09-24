4b2a5386643865af27f663323f51308f15012076
Feriz <ferizpeci@gmail.com>
2012-09-24 17:21:08 +0200

    Feriz och Christer har gjort user stories #6 i Sprint backlog v39. Programmet slumpar fram en teckenbild och visar den i spelfönstret, en ny teckenbild slumpas fram varje gång man klickar på next letter knappen. Den här tecknet visas i en nytillagd imageview i spelfönstret (Game.java).

--- Android/src/com/example/android/Game.java
+++ Android/src/com/example/android/Game.java
@@ -7,23 +7,38 @@ import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Game extends Activity {
-	@Override
+	
+	
+	 ImageView image; 
+	 ImageButton button;
+	 Button firstOptionButton;
+	 String str = randomLetter();
+	 @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
-        getActionBar().setDisplayHomeAsUpEnabled(true);        
+        getActionBar().setDisplayHomeAsUpEnabled(true);     
+        
+       
                
-        final ImageButton firstOptionButton = (ImageButton) findViewById(R.id.first_opt_button);
+        firstOptionButton = (Button) findViewById(R.id.first_opt_button);
         final ImageButton secondOptionButton = (ImageButton) findViewById(R.id.second_opt_button);
         final ImageButton thirdOptionButton = (ImageButton) findViewById(R.id.third_opt_button);        
-        final ImageButton nextLetterButton = (ImageButton) findViewById(R.id.next_letter_button);
+//        final ImageButton nextLetterButton = (ImageButton) findViewById(R.id.next_letter_button);
         
-
+        
+        
         
         firstOptionButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {							
@@ -38,22 +53,64 @@ public class Game extends Activity {
         thirdOptionButton.setOnClickListener(new View.OnClickListener() {
  			public void onClick(View v) {				
  			}
- 		});       
+ 		});  
         
-        nextLetterButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				startActivity(new Intent("android.intent.action.GAMEEND")); 				
-			}
-		});        
         
+        // Ändrar bild varje gång man klickar på next knappen
+        switchPic();
+        
+        // Ändrar text på knappen
+        firstOptionButton.setText(str);
+        
+//        nextLetterButton.setOnClickListener(new View.OnClickListener() {
+//			public void onClick(View v) {
+//					switchPic();
+//				}				
+//		});        
+         
     }
+	
+	
+	private void switchPic(){
+		image = (ImageView) findViewById(R.id.imageView);
+		button = (ImageButton) findViewById(R.id.next_letter_button);
+		image.setImageResource(picSetter(str));
+		
+		button.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				str = randomLetter();
+				firstOptionButton.setText(str);
+				image.setImageResource(picSetter(str));
+			} 
+		});
+	}
+	
+
+	
+	private String randomLetter()
+	{
+		Random rng = new Random();
+	
+		String randomLetter = "abcdefghijklmnopqrestuvwz";
+		String name = Character.toString(randomLetter.charAt(rng.nextInt(25)));
+		return name;
+	}
+	private int picSetter(String letter){
+		
+//		String randomLetter = "abcdefghijklmnopqrestuvwz";
+//		String name = Character.toString(randomNumber); // Ändra till 28 när vi lägger till å ä ö
+		
+		int resource = getResources().getIdentifier(letter, "drawable", "com.example.android");
+		return resource;
+		
+	}
 	
 	 @Override
 	 public boolean onCreateOptionsMenu(Menu menu) {
 	     getMenuInflater().inflate(R.menu.activity_game, menu);
 	     return true;
 	 }
-
+	 
 	    
 	 //@Override
 	 public boolean onOptionsItemSelected(MenuItem item) {

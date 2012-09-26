d54cebc7cb92a13b4b796a147aab09ea30150769
robinos <robin.l.osborne@gmail.com>
2012-09-26 11:52:17 +0200

    Revert "Har ändrat på layout i Game lite så timerBar syns tydlig (och bytt namn"
    This reverts commit e0365df4dc7f0e5a56e82e5af44935235870c59a.

--- Android/src/com/example/android/Game.java
+++ Android/src/com/example/android/Game.java
@@ -3,8 +3,6 @@ package com.example.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.os.Handler;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +11,6 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -21,16 +18,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-/**
- * Game
- * 
- * Note - next letter should be disabled until a right or wrong answer is
- * received (or time runs out).  Right now, clicking the next button
- * several times quickly gives strange behaviour, especially for the timer.
- * 
- * @author : Grupp02
- *
- */
 public class Game extends Activity {
 	
 	
@@ -38,13 +25,8 @@ public class Game extends Activity {
 	 ImageButton button;
 	 Button firstOptionButton;
 	 String str = randomLetter();
-	 ArrayList<String> usedSignList = new ArrayList<String>();
-	 private ProgressBar timerBar;
-     private CountDownTimer countDownTimer;	 
-     private int timeCount = 1;
-	 private int timeLimit = 11000; //11000 ms = 11s (needed instead of 10s)
-	 private int tickTime = 1000;  //1000 ms = 1s
-     private ImageButton nextLetterButton;
+	 ArrayList<String> usedSignList = new ArrayList<String>();	 
+	 
 	 
 	 @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,25 +37,27 @@ public class Game extends Activity {
        
                
         firstOptionButton = (Button) findViewById(R.id.first_opt_button);
-        //final ImageButton secondOptionButton = (ImageButton) findViewById(R.id.second_opt_button);
-        //final ImageButton thirdOptionButton = (ImageButton) findViewById(R.id.third_opt_button);        
-        timerBar = (ProgressBar) findViewById(R.id.timerBar);
-        nextLetterButton = (ImageButton) findViewById(R.id.next_letter_button); 
+        final ImageButton secondOptionButton = (ImageButton) findViewById(R.id.second_opt_button);
+        final ImageButton thirdOptionButton = (ImageButton) findViewById(R.id.third_opt_button);        
+//        final ImageButton nextLetterButton = (ImageButton) findViewById(R.id.next_letter_button);
+        
+        
+        
         
         firstOptionButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {							
 			}
 		});
         
-        //secondOptionButton.setOnClickListener(new View.OnClickListener() {
- 		//	public void onClick(View v) {				
- 		//	}
- 		//});
+        secondOptionButton.setOnClickListener(new View.OnClickListener() {
+ 			public void onClick(View v) {				
+ 			}
+ 		});
         
-        //thirdOptionButton.setOnClickListener(new View.OnClickListener() {
- 		//	public void onClick(View v) {				
- 		//	}
- 		//});  
+        thirdOptionButton.setOnClickListener(new View.OnClickListener() {
+ 			public void onClick(View v) {				
+ 			}
+ 		});  
         
         
         // Ändrar bild varje gång man klickar på next knappen
@@ -82,16 +66,11 @@ public class Game extends Activity {
         // Ändrar text på knappen
         firstOptionButton.setText(str);
         
-       //This is needed to restart the timer on clicking next letter
-       nextLetterButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				    timerBar.setProgress(0);				
-				    timeCount = 1;
-					startTimer();
-				}				
-		});
-        
-        startTimer();
+//        nextLetterButton.setOnClickListener(new View.OnClickListener() {
+//			public void onClick(View v) {
+//					switchPic();
+//				}				
+//		});        
          
     }
 	
@@ -161,27 +140,4 @@ public class Game extends Activity {
 	     return super.onOptionsItemSelected(item);
 	 }	
 
-	 /**
-	  * startTimer method starts the countdown timer for making a
-	  * choice in the game.
-	  * We can also later add code for cancel() on a right answer,
-	  * giving bonus points for time.
-	  */
-	 public void startTimer() {
-	     countDownTimer = new CountDownTimer(timeLimit,tickTime) {
-	    	//a long is required by onTick, timeLeft is not used
-	         public void onTick(long timeLeft) 
-	         {
-	        	 //The progress bar goes from 0 to 100 while timeCount
-	        	 //is 1 to 10, so *10 for display
-	             timerBar.setProgress(timeCount*10);
-	             timeCount++;
-	         }
-	        	 
-	         public void onFinish()
-	         {
-	             //on finish we can display (Fel!) or whatever we decide
-	         }
-	     }.start();		 
-	 }
 }

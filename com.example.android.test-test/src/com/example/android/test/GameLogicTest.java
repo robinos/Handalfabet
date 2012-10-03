a941383357601115199b661b7fa13091e5ea9cbf
christer7 <christer.evervall@gmail.com>
2012-10-03 15:30:47 +0200

    Ett nytt testfall för att se om svarsknapparna har unika svar.
    Detta test körs 10 gånger för att se om det blir nåt fel. Går inte 100
    ggr pga att det tar för lång tid.

--- com.example.android.test-test/src/com/example/android/test/GameLogicTest.java
+++ com.example.android.test-test/src/com/example/android/test/GameLogicTest.java
@@ -5,7 +5,14 @@ import com.example.android.*;
 
 import android.test.AndroidTestCase;
 
-
+/**
+ * The GameLogicTest class is a testclass to test the GameLogic for Game.
+ * 
+ * @author  : Grupp02
+ * @version : 2012-10-02, v0.2
+ * @License : GPLv3
+ *
+ */
 public class GameLogicTest extends AndroidTestCase {
 	
 	GameLogic gameLogic;
@@ -22,9 +29,30 @@ public class GameLogicTest extends AndroidTestCase {
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
-
+	
+	/**
+	 *  A simple test for score value
+	 */
 	public void simpleTestScore() {
 		
 		assertEquals(gameLogic.getRoundScore(),11);
+	}
+	/**
+	 * Test the tree answersbuttons gets unique strings. 
+	 */
+	public void testUniqueRandomizedLetters() {
+		gameLogic.determineChoices();
+		assertFalse(gameLogic.getFirstButtonString().equals(gameLogic.getSecondButtonString()));
+		assertFalse(gameLogic.getFirstButtonString().equals(gameLogic.getThirdButtonString()));
+		assertFalse(gameLogic.getSecondButtonString().equals(gameLogic.getThirdButtonString()));
+	}
+	
+	/**
+	 * Runs the tests method 10 times
+	 */
+	public void testUniqueRandomizedLetters10times() {
+		for(int i = 0; i < 10; i++) {
+			testUniqueRandomizedLetters();
+		}
 	}
 }

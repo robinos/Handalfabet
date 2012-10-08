96ea19103d1b37d435740b88c24c406451510130
christer7 <christer.evervall@gmail.com>
2012-10-08 14:20:36 +0200

    User.java filen hade olösta konfliketer som inte syntes i föregående merge

--- Android/src/com/example/android/User.java
+++ Android/src/com/example/android/User.java
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-package com.example.android;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 /**
  *   This file is part of Handalfabetet.
@@ -38,42 +27,7 @@ import android.widget.EditText;
  *
  */
 
-public class User extends Activity {
 
-	
-	
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.user);
-        
-        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            getActionBar().setDisplayHomeAsUpEnabled(true);
-        }	
-	}
-
-	
-	/** Skappar knappen högst upp i menyn */   
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_level_chooser, menu);
-        return true;
-    }
-
-	/** Går tillbaka till Main Activity när användaren klickar på knappen*/
-	 @Override
-	    public boolean onOptionsItemSelected(MenuItem item) {
-	        switch (item.getItemId()) {
-	            case android.R.id.home:
-	                NavUtils.navigateUpFromSameTask(this);
-	                return true;
-	        }
-	        return super.onOptionsItemSelected(item);
-	    }
-	 
-}
-=======
 package com.example.android;
 
 public class User {
@@ -129,4 +83,3 @@ public class User {
 	}
 }
 
->>>>>>> HighScore

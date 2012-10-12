7ff33b1b5029582c4ab323485286413992c44894
christer7 <christer.evervall@gmail.com>
2012-10-12 22:51:20 +0200

    GameLogicTest testar nu även logiken för nivå 2 och 3

--- com.example.android.test-test/src/com/example/android/test/GameLogicTest.java
+++ com.example.android.test-test/src/com/example/android/test/GameLogicTest.java
@@ -33,14 +33,17 @@ import android.util.Log;
  */
 public class GameLogicTest extends AndroidTestCase {
 	
-	GameLogic gameLogic;
+	GameLogic gameLogic1;
+	GameLogic gameLogic2;
+	GameLogic gameLogic3;
 	Game game;
 	
 
 	protected void setUp() throws Exception {
 		game = new Game();
-		gameLogic = new GameLogic(1,game);
-		
+		gameLogic1 = new GameLogic(1,game);
+		gameLogic2 = new GameLogic(2,game);
+		gameLogic3 = new GameLogic(3,game);
 		super.setUp();
 	}
 
@@ -50,10 +53,27 @@ public class GameLogicTest extends AndroidTestCase {
 	
 	/**
 	 *  A simple test for score value
+	 *  Difficult 1
 	 */
-	public void simpleTestScore() {
-		gameLogic.scoreCounter();
-		assertEquals(gameLogic.getRoundScore(), gameLogic.getTotalScore() );   
+	public void simpleTestScore1() {
+		gameLogic1.scoreCounter();
+		assertEquals(gameLogic1.getRoundScore(), gameLogic1.getTotalScore() );   
+	}
+	/**
+	 *  A simple test for score value
+	 *  Difficult 2
+	 */
+	public void simpleTestScore2() {
+		gameLogic2.scoreCounter();
+		assertEquals(gameLogic2.getRoundScore(), gameLogic2.getTotalScore() );   
+	}
+	/**
+	 *  A simple test for score value
+	 *  Difficult 3
+	 */
+	public void simpleTestScore3() {
+		gameLogic1.scoreCounter();
+		assertEquals(gameLogic3.getRoundScore(), gameLogic3.getTotalScore() );   
 	}
 	
 	/**
@@ -61,49 +81,53 @@ public class GameLogicTest extends AndroidTestCase {
 	 */
 	public void simpleTestScore10times() {
 		for(int i = 0; i < 10; i++) {
-			simpleTestScore();
+			simpleTestScore1();
 		}
 	}
 	
 	
 	/**
-	 * Tests if the tree answersbuttons gets unique strings. 
+	 * Tests if the tree answersbuttons gets unique strings.
+	 * Difficult 1 
 	 */
 	public void testUniqueRandomizedLetters() {
 		
-		gameLogic.determineChoices();
-		assertFalse(gameLogic.getFirstButtonString()
+		gameLogic1.determineChoices();
+		assertFalse(gameLogic1.getFirstButtonString()
 							 .equals(
-				    gameLogic.getSecondButtonString()));
+				    gameLogic1.getSecondButtonString()));
 		
-		assertFalse(gameLogic.getFirstButtonString()
+		assertFalse(gameLogic1.getFirstButtonString()
 							 .equals(
-					gameLogic.getThirdButtonString()));
+					gameLogic1.getThirdButtonString()));
 		
-		assertFalse(gameLogic.getSecondButtonString()
+		assertFalse(gameLogic1.getSecondButtonString()
 							 .equals(
-					gameLogic.getThirdButtonString()));
+					gameLogic1.getThirdButtonString()));
 		
 		
-		Log.e("",gameLogic.getFirstButtonString());
-		Log.e("",gameLogic.getSecondButtonString());
-		Log.e("",gameLogic.getThirdButtonString());
+		Log.e("",gameLogic1.getFirstButtonString());
+		Log.e("",gameLogic1.getSecondButtonString());
+		Log.e("",gameLogic1.getThirdButtonString());
 		
 		
 	}
 	
-	public void testIfAnswersButtonsContainsTheCorrextAnswer() {
+	/**
+	 * Difficult 1
+	 */
+	public void testIfAnswersButtonsContainsTheCorrextAnswer1() {
 		
-		gameLogic.determineChoices();
+		gameLogic1.determineChoices();
 		
-		Log.e("","<<<"+	gameLogic.getFirstPicture());
-		Log.e("","<<<"+	gameLogic.getFirstButtonString());
+		Log.e("","<<<"+	gameLogic1.getFirstPicture());
+		Log.e("","<<<"+	gameLogic1.getFirstButtonString());
 		
-		assertTrue(	gameLogic.getFirstButtonString().equals(gameLogic.getFirstPicture())
+		assertTrue(	gameLogic1.getFirstButtonString().equals(gameLogic1.getFirstPicture())
 					||
-					gameLogic.getSecondButtonString().equals(gameLogic.getFirstPicture()) 
+					gameLogic1.getSecondButtonString().equals(gameLogic1.getFirstPicture()) 
 					||
-					gameLogic.getThirdButtonString().equals(gameLogic.getFirstPicture()) 
+					gameLogic1.getThirdButtonString().equals(gameLogic1.getFirstPicture()) 
 					);
 		
 		
@@ -118,4 +142,122 @@ public class GameLogicTest extends AndroidTestCase {
 			testUniqueRandomizedLetters();
 		}
 	}
+	
+	/**
+	 * Tests if the tree answersbuttons gets unique strings. 
+	 * Difficult 2
+	 */
+	public void testUniqueRandomizedLetters2() {
+		
+		gameLogic2.determineChoices();
+		assertFalse(gameLogic2.getFirstButtonString()
+							 .equals(
+				    gameLogic2.getSecondButtonString()));
+		
+		assertFalse(gameLogic2.getFirstButtonString()
+							 .equals(
+					gameLogic2.getThirdButtonString()));
+		
+		assertFalse(gameLogic2.getSecondButtonString()
+							 .equals(
+					gameLogic2.getThirdButtonString()));
+		
+		
+		Log.e("",gameLogic2.getFirstButtonString());
+		Log.e("",gameLogic2.getSecondButtonString());
+		Log.e("",gameLogic2.getThirdButtonString());
+		
+		
+	}
+	
+	/**
+	 * Tests if the tree answersbuttons gets unique strings. 
+	 * Difficult 3 
+	 */
+	public void testUniqueRandomizedLetters3() {
+		
+		gameLogic3.determineChoices();
+		assertFalse(gameLogic3.getFirstButtonString()
+							 .equals(
+				    gameLogic3.getSecondButtonString()));
+		
+		assertFalse(gameLogic3.getFirstButtonString()
+							 .equals(
+					gameLogic3.getThirdButtonString()));
+		
+		assertFalse(gameLogic3.getSecondButtonString()
+							 .equals(
+					gameLogic3.getThirdButtonString()));
+		
+		
+		Log.e("",gameLogic3.getFirstButtonString());
+		Log.e("",gameLogic3.getSecondButtonString());
+		Log.e("",gameLogic3.getThirdButtonString());
+		
+		
+	}
+	/**
+	 * Difficult 2
+	 */
+	public void testIfAnswersButtonsContainsTheCorrextAnswer2() {
+		
+		gameLogic2.determineChoices();
+		
+		Log.e("","<<<"+	gameLogic2.getFirstPicture() 
+				      + gameLogic2.getSecondPicture());
+
+		Log.e("","<<<"+	gameLogic2.getFirstButtonString());
+		Log.e("","<<<"+	gameLogic2.getSecondButtonString());
+		Log.e("","<<<"+	gameLogic2.getThirdButtonString());
+		
+		assertTrue(	gameLogic2.getFirstButtonString().equals(
+					 		gameLogic2.getFirstPicture()
+					 		+gameLogic2.getSecondPicture())
+					||
+					gameLogic2.getSecondButtonString().equals(
+							 gameLogic2.getFirstPicture()
+							 +gameLogic2.getSecondPicture()) 
+					||
+					gameLogic2.getThirdButtonString().equals(
+							 gameLogic2.getFirstPicture()
+							 +gameLogic2.getSecondPicture()) 
+					);
+		
+		
+	}
+	
+	/**
+	 * Difficult 3
+	 */
+	public void testIfAnswersButtonsContainsTheCorrextAnswer3() {
+		
+		gameLogic3.determineChoices();
+		
+		Log.e("","<<<"+	gameLogic3.getFirstPicture()
+					  +	gameLogic3.getSecondPicture()
+					  + gameLogic3.getThirdPicture());
+		
+		Log.e("","<<<"+	gameLogic3.getFirstButtonString());
+		Log.e("","<<<"+	gameLogic3.getSecondButtonString());
+		Log.e("","<<<"+	gameLogic3.getThirdButtonString());
+		
+		assertTrue(	gameLogic3.getFirstButtonString().equals(
+							gameLogic3.getFirstPicture()
+							+gameLogic3.getSecondPicture()
+							+gameLogic3.getThirdPicture())
+					||
+					gameLogic3.getSecondButtonString().equals(
+							gameLogic3.getFirstPicture()
+							+gameLogic3.getSecondPicture()
+							+gameLogic3.getThirdPicture()) 
+					||
+					gameLogic3.getThirdButtonString().equals(
+							gameLogic3.getFirstPicture()
+							+gameLogic3.getSecondPicture()
+							+gameLogic3.getThirdPicture()) 
+					);
+		
+		
+	}
+
 }
